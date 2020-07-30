5ee9b78d71b6d35b7c3dbbcad9ac02c0483549b5
AngeloBastos <angelohbastos@gmail.com>
2020-07-30 13:20:22 -0300

    Exercise correction.

--- exercicio-05/app.js
+++ exercicio-05/app.js
@@ -49,8 +49,8 @@ while (i <= 5) {
   "Esta é a Xª vez que esta frase é exibida no console."
 */
 
-/* for (let i = 0; i <= 9; i++) {
-  console.log(`Esta é a ${i+1}ª vez que esta frase é exibida no console.`)
+/* for (let i = 1; i <= 10; i++) {
+  console.log(`Esta é a ${i}ª vez que esta frase é exibida no console.`)
 } */
 
 /*
