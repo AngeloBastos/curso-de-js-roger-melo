18f339c8787fa93862c717d264ee3f1643cef411
AngeloBastos <angelohbastos@gmail.com>
2021-01-15 16:10:42 -0300

    Exercise 17 is done.

--- exercicio-17/app.js
+++ exercicio-17/app.js
@@ -4,6 +4,33 @@
   - No envio do form, faça com que a página não seja recarregada.
 */
 
+const form = document.querySelector('form')
+
+const clearInput = () => {
+  input.value = ''
+  input.focus()
+}
+
+const logMessage = message => {
+  console.log(message)
+  clearInput()
+}
+
+const handleSubmit = event => {
+  event.preventDefault()
+  const input = event.target.input
+  const pattern = /^[a-zA-Z0-9]{7,11}$/
+  const isAValidValue = pattern.test(input.value)
+
+  if (isAValidValue) {
+    logMessage('O valor inserido no input é válido =)')
+    return
+  }
+  logMessage('Valor inválido =(')
+}
+
+form.addEventListener('submit', handleSubmit)
+
 /*
   02
 
@@ -19,6 +46,14 @@
   - Exiba no console o boolean no qual este teste resulta.
 */
 
+const paragraph = document.querySelector('p')
+
+
+const pattern = /documentation/
+const isAMatch = pattern.test(paragraph.textContent)
+
+console.log(isAMatch)
+
 /*
   04
 
@@ -28,6 +63,10 @@
 */
 
 const B99message = 'E o Terry Crews faz tudo, inclusive tocar a abertura de B99 na flauta'
+const B99Regex = /[A-Z0-9]{3}/
+const B99Result = B99Regex.test(B99message)
+
+console.log(B99Result)
 
 /*
   05
@@ -36,7 +75,7 @@ const B99message = 'E o Terry Crews faz tudo, inclusive tocar a abertura de B99 
     resultado do teste entre a regex e a string exibido no console seja true.
 */
 
-const word = 'O que a NASA fotografou no dia do seu aniversário?'
+const word = 'NASA'
 const NASARegex = /^[A-Z]{4}$/
 const NASAResult = NASARegex.test(word)
 
